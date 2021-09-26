ce0f995a651181e59bedf63e9b6f99ef3f87350d
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2021-09-26 01:13:18 -0300

    Salvando pedido, @PostMapping

--- src/main/java/br/estudandoemcasa/mvc/loja/controller/PedidoController.java
+++ src/main/java/br/estudandoemcasa/mvc/loja/controller/PedidoController.java
@@ -1,9 +1,15 @@
 package br.estudandoemcasa.mvc.loja.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.estudandoemcasa.mvc.loja.dto.PedidoDTO;
+import br.estudandoemcasa.mvc.loja.model.Pedido;
+import br.estudandoemcasa.mvc.loja.repositories.PedidoRepository;
 
 /*
  * Minhas Actions
@@ -12,9 +18,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/pedido")
 public class PedidoController {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
 
 	@GetMapping("/formulario")
 	public String formulario(Model resquest) {
+		return "pedido/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(PedidoDTO pedidoDTO) {
+		
+		Pedido pedido = pedidoDTO.toPedido();
+		pedidoRepository.save(pedido);
 		return "pedido/formulario";
 	}
 }
