81cdf24f46ff4a0acf3be729cafe392867f9fce1
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2021-09-25 21:18:43 -0300

    Criando a Classe Produto, Percorrendo a lista na view

--- src/main/java/br/estudandoemcasa/mvc/loja/controller/HomeController.java
+++ src/main/java/br/estudandoemcasa/mvc/loja/controller/HomeController.java
@@ -1,8 +1,13 @@
 package br.estudandoemcasa.mvc.loja.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.estudandoemcasa.mvc.loja.model.Pedido;
 
 /*
  * Minhas Actions
@@ -12,8 +17,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
 	@GetMapping("/home")
-	public String hello(Model resquest) {
-		//resquest.addAttribute("nameCampo", "Olá mundo Spring MVC!");
+	public String home(Model resquest) {
+
+		Pedido pedido = new Pedido();
+		pedido.setNome("Playstation 4");
+		pedido.setUrlImagem("https://images-americanas.b2w.io/produtos/01/00/img/3292904/4/3292904422_1SZ.jpg");
+		pedido.setUrlProduto("https://www.americanas.com.br/produto/3292904406?sellerId=00776574000660&epar=bp_pl_00_go_gm_jogos_todas_geral_gmv&opn=YSMESP&WT.srch=1&gclid=Cj0KCQjwkbuKBhDRARIsAALysV6IkzRYU5fzkuL1wgSx_QKGkZ3cqoAL5bf0YIFLWMvRNgAAcp1nCa0aApo3EALw_wcB");
+		pedido.setDescricao("Console Playstation 4 Hits 1tb Bundle 18 - Games God Of War + Ratchet And Clank + Ghost Of Tsushima");
+		
+		List<Pedido> pedidos = Arrays.asList(pedido);
+		resquest.addAttribute("pedidos", pedidos);
 		return "home";
 	}
 }
---
+++ src/main/java/br/estudandoemcasa/mvc/loja/model/Pedido.java
@@ -0,0 +1,56 @@
+package br.estudandoemcasa.mvc.loja.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Pedido implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String nome;
+	private BigDecimal valor;
+	private LocalDate dataEntrega;
+	private String urlProduto;
+	private String urlImagem;
+	private String descricao;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	public LocalDate getDataEntrega() {
+		return dataEntrega;
+	}
+	public void setDataEntrega(LocalDate dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}	
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
